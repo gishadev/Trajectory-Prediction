7a79ffc9a94acc843b7053b06e09c3e28cf7917e
GishaTheDeveloper <spaceshhz@gmail.com>
2020-09-14 23:24:22 +0300

    Add Turret.cs

---
+++ Trajectory-Prediction/Assets/Scripts/Turret.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    // PUBLIC_FIELDS //
+    public float rotationSpeed;
+    public float maxXRotation;
+    public float minXRotation;
+
+    // PRIVATE_FIELDS //
+    float yRot,xRot;
+
+    void Update()
+    {
+        Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")).normalized;
+
+        xRot += -input.z * rotationSpeed;
+        yRot += input.x * rotationSpeed;
+
+        xRot = Mathf.Clamp(xRot, -maxXRotation, -minXRotation);
+
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0f);
+    }
+}
